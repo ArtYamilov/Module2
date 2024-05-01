357a26b404e91faf235f6bb9c09d477b83bc3645
ArtYamilov <167341655+ArtYamilov@users.noreply.github.com>
2024-05-01 10:21:44 +0500

    Add files via upload

---
+++ Цели и задачи. Поток выполнения программы.py
@@ -0,0 +1,40 @@
+# from time import sleep
+#
+# a = 5
+# print(a)
+# print("Я тут")
+# sleep(4)
+# print("Фух, 4 секудны прошло")  # инструмент debug позволяет отследить логику выполнения программы.
+# идет однопоточное выполнение программы.
+# "probe"
+# print("дратути!")
+# # x = 34
+# # y = 43
+# # print(x * y)
+# # print("досвидания!")
+# my_family_height = [
+#     ['Мама', 168],
+#     ['Папа', 169],
+#     ['Я', 178],
+# ]
+# total_height = my_family_height[0][1]
+# total_height += my_family_height[1][1]
+# total_height += my_family_height[2][1]
+# print('Общий рост моей семьи', total_height)
+# print('досвидания!')
+#
+# my_cars_price = [
+#     ["Audi", 25000],
+#     ["Lexus", 30000],
+#     ["Toyota", 50000]
+# ]
+# total_price = my_cars_price[0][1]
+# total_price += my_cars_price[1][1]
+# total_price += my_cars_price[2][1]  # i=i+1, тоже самое i+=1 инкрементальная конкатенация в цикле.
+# print("Общая сумма моих автомобилей", total_price, "$")
+
+print("Hi, PyCharm")
+x = 43
+y = 32
+print(x * y)
+print("End line")
